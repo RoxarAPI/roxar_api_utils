d8e15bafb65c0b96936bc41a59679a613eaf52dd
Stein Gulbrandsen <stein.gulbrandsen@emerson.com>
2021-06-08 08:06:07 +0200

    make pip installable

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ functions that can be used to supplement the Roxar API.
 The libraries are found in the roxar_api_utils folder and sample example scripts
 are found in the examples folder.
 
+## Installation
+```sh
+roxenv python -m pip install git+https://github.com/RoxarAPI/roxar_api_utils#egg=roxar_api_utils
+```
+
+### History
 Originally written by Tor Barkve
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roxar_api_utils",
+    version="0.0.1",
+    author="Stein Gulbrandsen",
+    author_email="stein.gulbrandsen@emerson.com",
+    description="Common utility functions that can be used to supplement the Roxar API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RoxarAPI/roxar_api_utils",
+    project_urls={
+        "Bug Tracker": "https://github.com/RoxarAPI/roxar_api_utils/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["roxar_api_utils"],
+    python_requires=">=3.7",
+)
